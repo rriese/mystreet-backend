d11aeee0f8b205773910015a5e2878e4e3bbf779
rriese <rafael.riese@catolicasc.org.br>
2022-09-19 18:26:55 -0300

    Commit inicial

---
+++ src/main/java/com/github/riese/rafael/mystreet/MystreetApplication.java
@@ -0,0 +1,13 @@
+package com.github.riese.rafael.mystreet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MystreetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MystreetApplication.class, args);
+	}
+
+}
