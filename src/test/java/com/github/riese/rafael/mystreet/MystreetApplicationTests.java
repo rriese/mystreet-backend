ae8457987071ac6f8699f9268cd321fc7ee1320d
rriese <rafael.riese@catolicasc.org.br>
2022-11-10 18:50:20 -0300

    Criado teste

--- src/test/java/com/github/riese/rafael/mystreet/MystreetApplicationTests.java
+++ src/test/java/com/github/riese/rafael/mystreet/MystreetApplicationTests.java
@@ -1,13 +1,23 @@
 package com.github.riese.rafael.mystreet;
 
+import com.github.riese.rafael.mystreet.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class MystreetApplicationTests {
 
+	@Resource
+	UserService userService;
+
 	@Test
-	void contextLoads() {
+	void getUsersNotEmpty() {
+		var users = userService.findAll();
+		assertTrue(users.getBody().size() == 0);
 	}
 
 }
