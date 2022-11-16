a38f8a083e3c3a79179eea97d1a4aa371f13b3c6
rriese <rafael.riese@catolicasc.org.br>
2022-11-16 18:05:15 -0300

    Adicionado teste para status

---
+++ src/test/java/com/github/riese/rafael/mystreet/StatusServiceTests.java
@@ -0,0 +1,64 @@
+package com.github.riese.rafael.mystreet;
+
+import com.github.riese.rafael.mystreet.model.Status;
+import com.github.riese.rafael.mystreet.service.StatusService;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class StatusServiceTests {
+
+    @Resource
+    StatusService statusService;
+    static String statusId;
+
+    @BeforeAll
+    static void initTest() {
+        statusId = null;
+    }
+
+    @Test
+    @Order(1)
+    void createStatus() {
+        var status = new Status();
+        status.setName("User test");
+
+        Status statusCreated = null;
+
+        try {
+            statusCreated = statusService.save(status).getBody();
+            statusId = statusCreated.getId();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        assertNotNull(statusId);
+    }
+
+    @Test
+    @Order(2)
+    void findStatusCreated() {
+        var status = statusService.findById(statusId);
+        assertNotNull(status);
+    }
+
+    @Test
+    @Order(3)
+    void findAllStatus() {
+        var status = statusService.findAll().getBody();
+        assertTrue(status.size() > 0);
+    }
+
+    @Test
+    @Order(4)
+    void deleteStatus() {
+        var isStatusDeleted = statusService.delete(statusId).getBody();
+        assertTrue(isStatusDeleted);
+    }
+
+}
