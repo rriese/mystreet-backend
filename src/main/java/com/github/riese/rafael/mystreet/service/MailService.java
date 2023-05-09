e42d1fcd8b63812621fab3a9d238aa554f7e88d1
rriese <rafael.riese@catolicasc.org.br>
2023-05-08 21:49:57 -0300

    Test

--- src/main/java/com/github/riese/rafael/mystreet/service/MailService.java
+++ src/main/java/com/github/riese/rafael/mystreet/service/MailService.java
@@ -2,11 +2,11 @@ package com.github.riese.rafael.mystreet.service;
 
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 
-@Service
+@Component
 public class MailService {
     @Resource
     private JavaMailSender mailSender;
